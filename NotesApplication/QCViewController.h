98155c2f6f310d62101e0db79b5a2b042279ab8f
wshi27 <wshi27@hotmail.com>
2013-07-02 18:37:39 -0400

    Initial Commit

---
+++ NotesApplication/QCViewController.h
@@ -0,0 +1,13 @@
+//
+//  QCViewController.h
+//  NotesApplication
+//
+//  Created by Weiwei Shi on 7/2/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCViewController : UIViewController
+
+@end
